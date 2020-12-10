7266cb2b5785b5bbd643f06a5326aa2f7d380073
Keshav Garg <66558551+kahupihu@users.noreply.github.com>
2020-12-10 18:16:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(800,400);
+  createCanvas((window.width*2),window.height);
   muffin=createSprite(100,300,10,10);
   muffin.addAnimation("sleep",start);
   muffin.addAnimation("walking",walking);
